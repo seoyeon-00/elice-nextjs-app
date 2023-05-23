a277e4bf32d016cd54e2777ea0b06f4d0fb4ec56
seoyeon-00 <84628128+seoyeon-00@users.noreply.github.com>
2023-05-23 14:39:47 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+
+
+export default function Home() {
+  console.log("Home");
+  return (
+    <> 
+      <Head>
+        <title>WEB!</title>
+      </Head>
+      <h2></h2>
+      Hello, World!
+    </>
+  );
+}
